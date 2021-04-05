b5201ccd546edd8b12af0d6035a6921a3a675937
Brady Snelson <bsnelson@umd.edu>
2021-04-05 19:10:16 -0400

    added picamera

--- MMM-1-Second-A-Day.js
+++ MMM-1-Second-A-Day.js
@@ -73,6 +73,7 @@ Module.register('MMM-1-Second-A-Day',
 	recordClip: function () {
 		this.sendSocketNotification('RECORD_CLIP')
 
+		/*
 		const self = this;
 		navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function (stream) {
 			self.status = "STATUS_RECORDING";
@@ -103,5 +104,6 @@ Module.register('MMM-1-Second-A-Day',
 			}, 500);
 
 		});
+		*/
 	},
 });
