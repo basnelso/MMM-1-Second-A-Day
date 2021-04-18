895f3eeb43734938e6c25824e25c13f66182bbe7
Brady Snelson <bsnelson@umd.edu>
2021-04-17 21:06:46 -0400

    added picture

--- node_helper.js
+++ node_helper.js
@@ -95,7 +95,7 @@ module.exports = NodeHelper.create({
 				nopreview: false,
 				vflip: true,
 				fullscreen: false,
-				preview: '0,0,1920,1080'
+				preview: '0,0,1280,720'
 			});
 		} else if (orientation == 'vertical') {
 			myCamera = new PiCamera({
@@ -106,7 +106,7 @@ module.exports = NodeHelper.create({
 				nopreview: false,
 				vflip: true,
 				fullscreen: false,
-				preview: '0,0,1080,1920'
+				preview: '0,0,720,1280'
 			});
 		}
 
