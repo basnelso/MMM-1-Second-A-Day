3f74d2db4962f9a0144a3d64ce6d465a9dfa7703
Brady Snelson <bsnelson@umd.edu>
2021-06-25 11:36:56 -0500

    added pic uploading

--- node_helper.js
+++ node_helper.js
@@ -58,7 +58,7 @@ module.exports = NodeHelper.create({
 			} else {
 				files.forEach(function(file) {
 					console.log("Uploading " + file);
-					uploadUniqueFile(file, VIDEO_PATH + file, '14-i6Hvbqfw3wsBKhti9h_IMD-ty1sHsE', () => {
+					uploadUniqueFile(file, IMAGE_PATH + file, '14-i6Hvbqfw3wsBKhti9h_IMD-ty1sHsE', () => {
 						self.sendSocketNotification("STATUS_UPDATE", {
 							status: "STATUS_UPLOADED"
 						});
